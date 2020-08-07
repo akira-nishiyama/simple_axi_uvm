7c222d3ceb5f7a482ad3716ae354d5e107d40fa4
Akira Nishiyama <mr081677@gmail.com>
2020-08-08 07:22:49 +0900

    fix typo. fix incorrect uvm_info call.

--- include/simple_axi_master_base_sequence.svh
+++ include/simple_axi_master_base_sequence.svh
@@ -6,6 +6,7 @@ virtual class simple_axi_master_base_sequence extends uvm_sequence #(simple_axi_
         if(starting_phase != null) begin
             `uvm_info(get_type_name(),
                       $sformatf("%s pre_body() raising %s objection",
+                      get_sequence_path(),
                       starting_phase.get_name()), UVM_MEDIUM);
             starting_phase.raise_objection(this);
         end
@@ -15,7 +16,7 @@ virtual class simple_axi_master_base_sequence extends uvm_sequence #(simple_axi_
     virtual task post_body();
         if(starting_phase != null) begin
             `uvm_info(get_type_name(),
-                      $sformatf("%s post_bodu() dropping %s objection",
+                      $sformatf("%s post_body() dropping %s objection",
                                 get_sequence_path(),
                                 starting_phase.get_name()), UVM_MEDIUM);
         starting_phase.drop_objection(this);
